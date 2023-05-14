2f7815cb965781c8537fc9c410aefc61b6c4ccfb
Sergey Bogatov <moonygamesstudio@gmail.com>
2023-05-14 16:09:49 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from os import getenv
+
 import sqlite3
 import telebot
 
-bot = telebot.TeleBot("")
+TOKEN = getenv('BOT_TOKEN') if (getenv('BOT_TOKEN') is not None) else open('tokens.txt', 'r').readline().strip()
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Добро пожаловать')
+
+
+bot.polling(none_stop=True)
