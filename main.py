9c2aa7f3ba286f70b11c01c4822ed477b845518b
Sergey Bogatov <moonygamesstudio@gmail.com>
2023-05-14 18:14:16 +0300

    Complete registration check

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ cursor = conn.cursor()
 
 bot = telebot.TeleBot(TOKEN)
 
+full_name = ''
+policy_number = 0
+is_registered = False
+
+current_user = {'user_id': None, 'name': None, 'surname': None, 'patronymic': None, 'policy_number': None}
+
 
 def db_table_val(user_id: int, user_name: str, user_surname: str, user_patronymic: str, policy_number: int):
     cursor.execute('INSERT INTO patients (user_id, user_name, user_surname, user_patronymic, policy_number) '
@@ -18,13 +24,47 @@ def db_table_val(user_id: int, user_name: str, user_surname: str, user_patronymi
     conn.commit()
 
 
+def db_find_val(id: int):
+    cursor.execute("SELECT user_id FROM patients WHERE user_id = '{}'".format(id))
+    result = cursor.fetchall()
+
+    if len(result) > 0:
+        return True
+    else:
+        return False
+
+
+def db_get_user_data_by_id(id: int):
+    global current_user
+
+    cursor.execute("SELECT * FROM patients WHERE user_id = '{}'".format(id))
+    row = cursor.fetchone()
+
+    if row is not None:
+        current_user['user_id'] = row[1]
+        current_user['name'] = row[2]
+        current_user['surname'] = row[3]
+        current_user['patronymic'] = row[4]
+        current_user['policy number'] = row[5]
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать!')
+    global is_registered
+    global current_user
 
+    current_user['user_id'] = message.from_user.id
 
-full_name = ''
-policy_number = 0
+    if db_find_val(current_user.get('user_id')):
+        is_registered = True
+
+        db_get_user_data_by_id(current_user.get('user_id'))
+
+        bot.send_message(message.chat.id, 'Добро пожаловать, {}!'.format(current_user.get('name')))
+    else:
+        is_registered = False
+
+        bot.send_message(message.chat.id, 'Добро пожаловать!')
 
 
 @bot.message_handler(content_types=['text'])
@@ -44,6 +84,8 @@ def get_name(message):
 
 def get_policy_number(message):
     global policy_number
+    global full_name
+    global current_user
 
     while policy_number == 0:
         try:
@@ -51,15 +93,16 @@ def get_policy_number(message):
         except ValueError:
             bot.send_message(message.from_user.id, 'Введите действительный номер!')
 
-    global full_name
-
     processed_full_name = full_name.split(' ')
 
-    us_surname = processed_full_name[0]
-    us_name = processed_full_name[1]
-    us_patronymic = processed_full_name[2]
+    current_user['surname'] = processed_full_name[0]
+    current_user['name'] = processed_full_name[1]
+    current_user['patronymic'] = processed_full_name[2]
 
-    db_table_val(user_id=message.from_user.id, user_name=us_name, user_surname=us_surname, user_patronymic=us_patronymic, policy_number=policy_number)
+    current_user['policy_number'] = policy_number
+
+    db_table_val(user_id=current_user.get('user_id'), user_name=current_user.get('name'), user_surname=current_user.get('surname'),
+                 user_patronymic=current_user.get('patronymic'), policy_number=current_user.get('policy_number'))
 
     bot.send_message(message.from_user.id, 'Регистрация окончена!')
 
